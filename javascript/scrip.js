9926c8a458b50bd5c9643c5b8114e1d6a8b12aeb
TatianaCotruta <cotruta.tati@gmail.com>
2024-01-18 15:43:20 +0100

    Add homeworks-js

--- javascript/scrip.js
+++ javascript/scrip.js
@@ -1,7 +1,7 @@
 //Homework - js - 1
 
 
-const country = "Republic of Moldova";
+/*const country = "Republic of Moldova";
 const continent = "Europe";
 let population = 2.6;
 const unit_population = "millions";
@@ -29,4 +29,111 @@ console.log(false !== !true);   //false negare-egal(nu este egal) negare true(fa
 console.log(true === 4);    //true === false = false
 console.log(false === (847 === '847')); //false === false = true
 console.log(false === (847 == '847'));  //false ===true = false
-console.log((!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false); //false or false or true or true = true
+console.log((!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false); //false or false or true or true = true */
+
+
+//Homework - js - 3
+
+/*const weightAna = 65;
+const heightAna = 1.75;
+
+const anaBMI = (weightAna / (heightAna ** 2)).toFixed(2);
+console.log(anaBMI);
+
+if (anaBMI < 18.5) {
+    console.log(`Ana is underweight`);
+} else if (anaBMI >= 18.5 && anaBMI <= 25) {
+    console.log(`Ana is normal weight`);}
+
+
+const weightBob = 93;
+const heightBob = 1.89;
+
+const bobBMI = (weightBob / (heightBob ** 2)).toFixed(2);
+console.log(bobBMI);
+
+if (bobBMI < 18.5 || bobBMI > 25) {
+    console.log(`Bob is underweight or overweight`);
+} else { 
+    console.log("Bob is normal weight");
+}*/
+
+
+//Homework - js- 4
+const calculateSupply = (vârsta, cantitateaZilnica) => {
+    const cantitateaTotala = cantitateaZilnica * vârsta; 
+    return (`You will need ${cantitateaTotala} to last you until the ripe old age of ${vârsta}`);
+}
+    console.log(calculateSupply(80, 3));
+
+
+const checkOddEven = (numar) => {
+    if (numar % 2 === 0) {
+        return (`Numarul este par`);
+    }
+    else if (numar % 2 === 1) {
+        return ("Numarul este impar");
+    }
+}
+
+
+const fizzBuzz = (numar) => {
+    if (numar % 3 == 0) {
+        return ("Fizz");
+    }
+    if (numar % 5 == 0) {
+        return ("Buzz");
+    }
+    if (numar % 3 == 0 && numar % 5 == 0) {
+        return ("FizzBuzz");
+    }
+}
+
+  
+
+
+/*Homework - js - 5
+
+const fruits = [];
+fruits.push(`apple`, `banana`, `orange`);
+console.log(fruits.length);
+console.log(fruits);
+
+const numbers = [1, 2, 3, 4, 5];
+const removedNumbers = numbers.pop();
+console.log(removedNumbers);
+
+const colors = [`red`, `blue`, `green`];
+const colorsIndex = colors.indexOf(`green`);
+console.log(colorsIndex);
+const includesOrange = colors.includes(`orange`);
+console.log(includesOrange);
+
+const names = ["John", "Jane", "Mike", 
+"Anna"];
+names.unshift("David");
+console.log(names);
+const namesIndex = names.indexOf("Mike");
+console.log(namesIndex);
+
+const numbers2 = [2, 4, 6, 8, 10];
+const includes = numbers2.includes(5);
+console.log(includes);
+numbers2.push(12);
+console.log(numbers2);
+
+const fruits2 = [`apple`, `banana`, `orange`];
+
+for (let i = 0; i < fruits2.length; i++) {
+    console.log(fruits2[i]);
+} */
+
+
+
+
+
+ 
+
+
+
+
